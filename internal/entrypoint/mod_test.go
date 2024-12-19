fa3a201f94722bf897325e1e287923863938e385
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-12-19 13:46:29 +0100

    modify test

--- internal/entrypoint/mod_test.go
+++ internal/entrypoint/mod_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestExampleFunction(t *testing.T) {
-	// Example test case
-	result := businessLogic(1, 2)
-	assert.Equal(t, 3, result)
+	assert.Equal(t, 3, businessLogic(1, 2))
+	assert.Equal(t, 5, businessLogic(1, 4))
 }

fbbbeb57dc8df63817eb29250b65037aead677dc
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-12-19 13:33:14 +0100

    add business logic

--- internal/entrypoint/entrypoint.go
+++ internal/entrypoint/entrypoint.go
@@ -2,6 +2,11 @@ package entrypoint
 
 import "fmt"
 
+func businessLogic(a, b int) int {
+	return a + b
+}
+
 func Entrypoint() {
 	fmt.Println("Hello, World!")
+	fmt.Println("1+2=", businessLogic(1, 2))
 }

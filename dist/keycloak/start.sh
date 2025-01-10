2358167ccdaba44df9e23b08729f2d20ed5f2c5b
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-01-10 21:57:07 +0100

    also inject user password dynamically

--- dist/keycloak/start.sh
+++ dist/keycloak/start.sh
@@ -5,12 +5,14 @@ echo "Running custom init script to inject secrets into realm-config.json"
 
 cd /tmp
 
-jq "(.clients[] | select(.clientId == \"${OIDC_CLIENT_ID}\")).secret = \"${OIDC_CLIENT_SECRET}\"" base-realm-config.json > update.json
-jq "(.clients[] | select(.clientId == \"${OIDC_CLIENT_ID}\")).secret = \"${OIDC_CLIENT_SECRET}\"" update.json > update2.json
+# We can't put this change in a separate config file and merge it because it's using and array and idk how to merge arrays based on a key (clientId)
+jq "(.clients[] | select(.clientId == \"${OIDC_CLIENT_ID}\")).secret = \"${OIDC_CLIENT_SECRET}\"" base-realm-config.json > tmp1.json
+# This change on the other hand can, since the "users" key doesn't even exist in the base config
+jq -s '.[0] * .[1]' tmp1.json users-realm-config.json > tmp2.json
 
 IMPORT_DIR=/opt/keycloak/data/import/
 mkdir -p $IMPORT_DIR
-cp update2.json $IMPORT_DIR/realm-config.json
+cp tmp2.json $IMPORT_DIR/realm-config.json
 
 echo "Running custom script finished !"
 export KEYCLOAK_IMPORT=$IMPORT_DIR/realm-config.json

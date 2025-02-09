0f887754beba2ad8b87364ad7508abf76f1ff622
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-02-09 19:25:28 +0100

    use library to simply getting config options from env

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	dario.cat/mergo v1.0.0 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/ProtonMail/go-crypto v1.1.5 // indirect
+	github.com/caarlos0/env/v11 v11.3.1 // indirect
 	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/cyphar/filepath-securejoin v0.3.6 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
--- internal/backend/config/config.go
+++ internal/backend/config/config.go
@@ -3,24 +3,22 @@ package config
 
 import (
 	"fmt"
-	"os"
-	"slices"
-	"strconv"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	VERBOSE                bool
-	REPO_FETCH_PERIOD_SECS int
-	REPO_DIR               string
-	DB_URL                 string
-	OIDC_BASE_URL          string
-	OIDC_CLIENT_ID         string
-	OIDC_CLIENT_SECRET     string
-	OIDC_USER_ID           string
-	OIDC_USER_PASSWORD     string
-	OIDC_REALM             string
+	VERBOSE                bool   `env:"VERBOSE" envDefault:"false"`
+	REPO_FETCH_PERIOD_SECS int    `env:"REPO_FETCH_PERIOD_SECS"`
+	REPO_DIR               string `env:"REPO_DIR"`
+	DB_URL                 string `env:"DB_URL"`
+	OIDC_BASE_URL          string `env:"OIDC_BASE_URL"`
+	OIDC_CLIENT_ID         string `env:"OIDC_CLIENT_ID"`
+	OIDC_CLIENT_SECRET     string `env:"OIDC_CLIENT_SECRET"`
+	OIDC_USER_ID           string `env:"OIDC_USER_ID"`
+	OIDC_USER_PASSWORD     string `env:"OIDC_USER_PASSWORD"`
+	OIDC_REALM             string `env:"OIDC_REALM"`
 }
 
 var configInst *Config
@@ -33,30 +31,19 @@ func Get() Config {
 	return *configInst
 }
 
-func getEnvInt(key string) int {
-	val, err := strconv.Atoi(os.Getenv(key))
-	if err != nil {
-		panic(fmt.Sprintf("Invalid value for %s: %v", key, err))
-	}
-	return val
-}
-
 func Init() {
 	err := godotenv.Load()
 	if err != nil {
 		// logrus not setup yet
 		fmt.Printf("Did not load .env file (%v)\n", err)
 	}
-	configInst = &Config{
-		VERBOSE:                !slices.Contains([]string{"false", "0", ""}, os.Getenv("VERBOSE")),
-		REPO_FETCH_PERIOD_SECS: getEnvInt("REPO_FETCH_PERIOD_SECS"),
-		REPO_DIR:               os.Getenv("REPO_DIR"),
-		DB_URL:                 os.Getenv("DB_URL"),
-		OIDC_BASE_URL:          os.Getenv("OIDC_BASE_URL"),
-		OIDC_USER_ID:           os.Getenv("OIDC_USER_ID"),
-		OIDC_USER_PASSWORD:     os.Getenv("OIDC_USER_PASSWORD"),
-		OIDC_REALM:             os.Getenv("OIDC_REALM"),
-		OIDC_CLIENT_ID:         os.Getenv("OIDC_CLIENT_ID"),
-		OIDC_CLIENT_SECRET:     os.Getenv("OIDC_CLIENT_SECRET"),
+
+	c := &Config{}
+	err = env.ParseWithOptions(c, env.Options{
+		RequiredIfNoDef: true,
+	})
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing config: %v", err))
 	}
+	configInst = c
 }

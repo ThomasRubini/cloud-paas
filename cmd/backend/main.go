63960e23c3731cb1a585dc793cf600cda69c6c83
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-03-03 21:58:08 +0100

    remove test code

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/ThomasRubini/cloud-paas/internal/paas_backend"
-	"github.com/ThomasRubini/cloud-paas/internal/paas_backend/imgbuild"
 )
 
 func main() {
-	err := imgbuild.Build("/home/itrooz/tmp2", "test")
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	paas_backend.Entrypoint()
 }

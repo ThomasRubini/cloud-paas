368cc1a0132f22b61009669e6236c4677968c323
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-03-04 21:46:17 +0100

    add CopyFields()

--- internal/utils/mod.go
+++ internal/utils/mod.go
@@ -1,5 +1,19 @@
 package utils
 
+import "reflect"
+
 func IsStatusCodeOk(statusCode int) bool {
 	return statusCode >= 200 && statusCode < 300
+}
+
+func CopyFields[A, B any](src *A, dst *B) {
+	srcValue := reflect.ValueOf(src).Elem()
+	dstValue := reflect.ValueOf(dst).Elem()
+
+	for i := 0; i < srcValue.NumField(); i++ {
+		dstField := dstValue.FieldByName(srcValue.Type().Field(i).Name)
+		if dstField.IsValid() {
+			dstField.Set(srcValue.Field(i))
+		}
+	}
 }

62d88a11cabf86a7bad4f3004e0d84afd61460c9
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-03-04 21:47:08 +0100

    improve GET and POST app endpoints

--- internal/paas_backend/endpoints/applications.go
+++ internal/paas_backend/endpoints/applications.go
@@ -3,6 +3,8 @@ package endpoints
 import (
 	"github.com/ThomasRubini/cloud-paas/internal/paas_backend/models"
 	"github.com/ThomasRubini/cloud-paas/internal/paas_backend/state"
+	"github.com/ThomasRubini/cloud-paas/internal/utils"
+	"github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,13 +14,21 @@ func initApplications(g *gin.RouterGroup) {
 	g.POST("/applications", createApp)
 }
 
+type AppView struct {
+	ID         uint   `json:"id"`
+	Name       string `json:"name"`
+	Desc       string `json:"desc"`
+	SourceURL  string `json:"source_url"`
+	AutoDeploy bool   `json:"auto_deploy"`
+}
+
 // GetApplications godoc
 // @Summary      List applications you have access to
 // @Tags         applications
 // @Produce      json
 // @Success      200
 // @Router       /api/v1/applications [get]
-// @Success      200 {array} models.DBApplication
+// @Success      200 {array} AppView
 func getApps(c *gin.Context) {
 
 	var apps []models.DBApplication
@@ -28,28 +38,53 @@ func getApps(c *gin.Context) {
 		return
 	}
 
-	c.JSON(200, apps)
+	var appsViews []AppView
+	for _, app := range apps {
+		var appView AppView
+		utils.CopyFields(&app, &appView)
+		appsViews = append(appsViews, appView)
+	}
+
+	c.JSON(200, appsViews)
+}
+
+type IdResponse struct {
+	ID uint `json:"id"`
+}
+
+type CreateAppRequest struct {
+	Name           string
+	Desc           string
+	SourceURL      string
+	SourceUsername string
+	SourcePassword string
+	AutoDeploy     bool
 }
 
 // Postapplications godoc
 // @Summary      Create a new application
 // @Tags         applications
 // @Accept       json
-// @Param        application body models.DBApplication true "application to create"
-// @Success      200
+// @Param        application body CreateAppRequest true "application to create"
+// @Success      200 {object} IdResponse
 // @Router       /api/v1/applications [post]
 func createApp(c *gin.Context) {
-	var newApp models.DBApplication
+	var request CreateAppRequest
 
-	if err := c.ShouldBindJSON(&newApp); err != nil {
+	if err := c.ShouldBindJSON(&request); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 
-	if err := state.Get().Db.Create(&newApp).Error; err != nil {
+	var newApp models.DBApplication
+	utils.CopyFields(&request, &newApp)
+
+	resp := state.Get().Db.Create(&newApp)
+	if err := resp.Error; err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
 
-	c.Status(200)
+	logrus.Debugf("Created new application with ID %d", newApp.ID)
+	c.JSON(200, IdResponse{ID: newApp.ID})
 }

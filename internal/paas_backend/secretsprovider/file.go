759cbd5e2e3cbe9f5cbe98e3882a3ebc35166204
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-03-04 23:22:15 +0100

    move file secrets provider in another file

---
+++ internal/paas_backend/secretsprovider/file.go
@@ -0,0 +1,89 @@
+package secretsprovider
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type FileSecretsProvider struct {
+	filePath string
+}
+
+func FromFile(filePath string) *FileSecretsProvider {
+	return &FileSecretsProvider{filePath: filePath}
+}
+
+func (fsp *FileSecretsProvider) readData() (map[string]string, error) {
+	data, err := os.ReadFile(fsp.filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return make(map[string]string), nil
+		}
+		return nil, err
+	}
+
+	secrets := make(map[string]string)
+	err = json.Unmarshal(data, &secrets)
+	if err != nil {
+		return nil, err
+	}
+
+	return secrets, nil
+}
+
+func (fsp *FileSecretsProvider) SetSecret(key, value string) error {
+	secrets, err := fsp.readData()
+	if err != nil {
+		return fmt.Errorf("could not read secrets file: %v", err)
+	}
+
+	secrets[key] = value
+
+	data, err := json.Marshal(secrets)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fsp.filePath, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fsp *FileSecretsProvider) GetSecret(key string) (string, error) {
+	secrets, err := fsp.readData()
+	if err != nil {
+		return "", fmt.Errorf("could not read secrets file: %v", err)
+	}
+
+	secret, ok := secrets[key]
+	if !ok {
+		return "", nil
+	}
+
+	return secret, nil
+}
+
+func (fsp *FileSecretsProvider) DeleteSecret(key string) error {
+	secrets, err := fsp.readData()
+	if err != nil {
+		return fmt.Errorf("could not read secrets file: %v", err)
+	}
+
+	delete(secrets, key)
+
+	data, err := json.Marshal(secrets)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fsp.filePath, data, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

d21f6499f668dc93bb64b94d0d7ad786e3dce482
Damien RODRIGUEZ <damien.rodriguez02@etu.umontpellier.fr>
2025-03-09 21:44:13 +0100

    "refactor" cli files and add all planned commands and actions

--- internal/paas_cli/cmd.go
+++ internal/paas_cli/cmd.go
@@ -1,59 +1,8 @@
 package paas_cli
 
 import (
-	"context"
-
 	"github.com/urfave/cli/v3"
 )
-
-var loginCmd = &cli.Command{
-	Name:   "login",
-	Usage:  "login with your account",
-	Action: LoginAction,
-}
-
-var registerCmd = &cli.Command{
-	Name:  "register",
-	Usage: "Register an account against the PaaS",
-	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:    "user",
-			Usage:   "Username for the account",
-			Aliases: []string{"u", "username"},
-		},
-		&cli.StringFlag{
-			Name:    "password",
-			Usage:   "Password for the account. Warning: this option and will log the password to your shell history. Prefer using stdin to input the password",
-			Aliases: []string{"p", "pass"},
-		},
-	},
-	Action: RegisterAction,
-}
-
-var projectRegisterCmd = &cli.Command{
-	Name:  "register",
-	Usage: "Register a new project",
-	Action: func(ctx context.Context, c *cli.Command) error {
-		println("project register called")
-		return nil
-	},
-}
-
-var projectListCmd = &cli.Command{
-	Name:  "list",
-	Usage: "List all projects",
-	Action: func(ctx context.Context, c *cli.Command) error {
-		println("project list called")
-		return nil
-	},
-}
-
-var projectCmd = &cli.Command{
-	Commands: []*cli.Command{
-		projectRegisterCmd,
-		projectListCmd,
-	},
-}
 
 var RootCmd = &cli.Command{
 	Flags: []cli.Flag{
@@ -64,9 +13,10 @@ var RootCmd = &cli.Command{
 		},
 	},
 	Commands: []*cli.Command{
-		registerCmd,
-		loginCmd,
-		projectCmd,
+		AccountCmd,
+		AppCmd,
+		EnvCmd,
+		TokenCmd,
 	},
 
 	Before: rootBefore,
---
+++ internal/paas_cli/env.go
@@ -0,0 +1,87 @@
+package paas_cli
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/urfave/cli/v3"
+)
+
+var appName = ""
+
+var EnvCmd = &cli.Command{
+	Name:   "env",
+	Usage:  "Interact with users applications environments",
+	Action: EnvCmdAction,
+}
+
+var subEnvCmd = &cli.Command{
+	Commands: []*cli.Command{
+		envCreateCmd,
+		envListCmd,
+		envInfoCmd,
+		envEditCmd,
+		envDeleteCmd,
+	},
+}
+
+var envCreateCmd = &cli.Command{
+	Name:   "create",
+	Usage:  "Create an environment for given application",
+	Action: createEnvAction,
+}
+
+var envListCmd = &cli.Command{
+	Name:   "list",
+	Usage:  "List all environments of a specific application",
+	Action: GetEnvListAction,
+}
+
+var envInfoCmd = &cli.Command{
+	Name:   "info",
+	Usage:  "Get information about a specific environment from a given application",
+	Action: GetEnvInfoAction,
+}
+
+var envEditCmd = &cli.Command{
+	Name:   "edit",
+	Usage:  "Edit a given environment from a given application",
+	Action: editEnvAction,
+}
+
+var envDeleteCmd = &cli.Command{
+	Name:   "delete",
+	Usage:  "Remove a given environment from a given application",
+	Action: deleteEnvAction,
+}
+
+func EnvCmdAction(ctx context.Context, cmd *cli.Command) error {
+	appName = cmd.Args().First()
+	subEnvCmd.Run(ctx, cmd.Args().Slice())
+	return nil
+}
+
+func createEnvAction(ctx context.Context, cmd *cli.Command) error {
+	fmt.Printf("Creating env %s for application %s...\n", cmd.Args().First(), appName)
+	return nil
+}
+
+func GetEnvListAction(ctx context.Context, cmd *cli.Command) error {
+	fmt.Printf("Listing all environments for application %s...\n", appName)
+	return nil
+}
+
+func GetEnvInfoAction(ctx context.Context, cmd *cli.Command) error {
+	fmt.Printf("Getting env %s informations for application %s...\n", cmd.Args().First(), appName)
+	return nil
+}
+
+func editEnvAction(ctx context.Context, cmd *cli.Command) error {
+	fmt.Printf("Editing env %s for application %s...\n", cmd.Args().First(), appName)
+	return nil
+}
+
+func deleteEnvAction(ctx context.Context, cmd *cli.Command) error {
+	fmt.Printf("Deleting env %s for application %s...\n", cmd.Args().First(), appName)
+	return nil
+}

b7bbf856e593493194b2e98761c711cc68fba2f2
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-03-10 21:49:22 +0100

    do not include DeletedAt column

--- internal/paas_backend/models/mod.go
+++ internal/paas_backend/models/mod.go
@@ -3,13 +3,10 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type BaseModel struct {
 	ID        uint `gorm:"primarykey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
 }

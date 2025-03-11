2803bc561ec62acc1b63b3a2aae08fcd4544db2d
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-03-11 21:49:54 +0100

    refactor to helpers file

---
+++ internal/paas_backend/tests/helpers.go
@@ -0,0 +1,65 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ThomasRubini/cloud-paas/internal/paas_backend"
+	"github.com/ThomasRubini/cloud-paas/internal/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func fakeState() utils.State {
+	gorm_db, err := gorm.Open(sqlite.Open(":memory:"))
+	if err != nil {
+		panic(err)
+	}
+
+	if err := paas_backend.MigrateModels(gorm_db); err != nil {
+		panic(err)
+	}
+
+	return utils.State{
+		Db: gorm_db,
+	}
+}
+
+func makeRequest(webServer *gin.Engine, method string, path string, body io.Reader) *httptest.ResponseRecorder {
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(method, path, body)
+	webServer.ServeHTTP(w, r)
+	return w
+}
+
+func makeOKRequest(t *testing.T, webServer *gin.Engine, method string, path string, body io.Reader) *httptest.ResponseRecorder {
+	w := makeRequest(webServer, method, path, body)
+	if w.Code != 200 {
+		fmt.Println("Request failed !")
+		fmt.Println("Body:")
+		fmt.Println(w.Body)
+		assert.Equal(t, 200, w.Code)
+	}
+	return w
+}
+
+func toJson(v interface{}) io.Reader {
+	b, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return bytes.NewBuffer(b)
+}
+
+func fromJson(body io.Reader, v interface{}) {
+	err := json.NewDecoder(body).Decode(v)
+	if err != nil {
+		panic(err)
+	}
+}

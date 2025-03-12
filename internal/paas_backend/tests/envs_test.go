58602c6b2aa5a2515df788340080cf9fbd8b3c2a
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-03-12 12:22:17 +0100

    add TestGetNoEnvs()

---
+++ internal/paas_backend/tests/envs_test.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ThomasRubini/cloud-paas/internal/comm"
+	"github.com/ThomasRubini/cloud-paas/internal/paas_backend"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetNoEnvs(t *testing.T) {
+	webServer := paas_backend.SetupWebServer(fakeState())
+
+	newApp := comm.CreateAppRequest{
+		Name: "test1",
+	}
+	makeOKRequest(t, webServer, "POST", "/api/v1/applications", toJson(newApp))
+
+	w := makeOKRequest(t, webServer, "GET", fmt.Sprintf("/api/v1/applications/%v/environments/", newApp.Name), nil)
+
+	var envs = fromJson[[]comm.EnvView](w.Body)
+	assert.Equal(t, 0, len(envs))
+}

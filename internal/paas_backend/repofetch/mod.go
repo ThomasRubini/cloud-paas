a6983601611236c58306973a8e51ef07975f58a4
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2025-04-07 08:35:45 +0200

    refactor: pull -> fetch

--- internal/paas_backend/repofetch/mod.go
+++ internal/paas_backend/repofetch/mod.go
@@ -23,7 +23,7 @@ func isDir(path string) bool {
 	return stat.IsDir()
 }
 
-// called on every repository on a schedule to pull them and update them
+// called on every repository on a schedule to fetch them and update them
 func FetchAndDeployRepository(state utils.State, project models.DBApplication) error {
 	if project.SourceURL == "" {
 		logrus.Infof("Skipping %s (empty source URL)", project.Name)
@@ -65,7 +65,7 @@ func FetchAndDeployRepository(state utils.State, project models.DBApplication) e
 }
 
 func handleRepositories() error {
-	logrus.Info("Pulling repositories due to recurring task")
+	logrus.Info("Fetching repositories due to recurring task")
 
 	// Get state
 	state := utils.GetState()
@@ -75,10 +75,10 @@ func handleRepositories() error {
 	if res.Error != nil {
 		return fmt.Errorf("error fetching project names: %w", res.Error)
 	}
-	logrus.Infof("Found %d projects to pull", len(projects))
+	logrus.Infof("Found %d projects to fetch", len(projects))
 
 	for _, project := range projects {
-		logrus.Debugf("Handling pulling project %v", project.Name)
+		logrus.Debugf("Handling fetching project %v", project.Name)
 		err := FetchAndDeployRepository(state, project)
 		if err != nil {
 			logrus.Errorf("error handling cron update for project %s: %v", project.Name, err)
@@ -125,9 +125,9 @@ func Init(period int) {
 		for {
 			err := handleRepositories()
 			if err != nil {
-				logrus.Errorf("Error pulling repositories: %v", err)
+				logrus.Errorf("Error fetching repositories: %v", err)
 			}
-			logrus.Debugf("Finished pulling repositories, sleeping for %d seconds", period)
+			logrus.Debugf("Finished fetching repositories, sleeping for %d seconds", period)
 
 			time.Sleep(time.Duration(period) * time.Second)
 		}

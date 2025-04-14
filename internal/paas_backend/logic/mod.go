377c04f3bae04a52dd71bc5357f80d82a71e8e5c
Damien RODRIGUEZ <damien.rodriguez02@etu.umontpellier.fr>
2025-04-14 23:36:56 +0200

    fix Environment Update crashing when env has no vars

--- internal/paas_backend/logic/mod.go
+++ internal/paas_backend/logic/mod.go
@@ -35,16 +35,18 @@ func (l LogicImpl) HandleEnvironmentUpdate(app models.DBApplication, env models.
 	port := imgbuild.GetExposedPort(l.State.DockerClient, imageTag)
 
 	parsedEnv := make(map[string]any)
-	err = json.Unmarshal([]byte(env.EnvVars), &parsedEnv)
-	if err != nil {
-		return fmt.Errorf("error unmarshalling environment variables: %w", err)
-	}
 	parsedEnvStr := make(map[string]string)
-	//avoid type misparsing from Kubernetes
-	for k, v := range parsedEnv {
-		parsedEnvStr[k] = fmt.Sprintf("%v", v)
+	if env.EnvVars != "" {
+		err = json.Unmarshal([]byte(env.EnvVars), &parsedEnv)
+		if err != nil {
+			return fmt.Errorf("error unmarshalling environment variables: %w", err)
+		}
+		parsedEnvStr := make(map[string]string)
+		//avoid type misparsing from Kubernetes
+		for k, v := range parsedEnv {
+			parsedEnvStr[k] = fmt.Sprintf("%v", v)
+		}
 	}
-
 	// Redeploy to kubernetes using the new image
 	err = deploy.DeployEnv(env, deploy.Options{
 		ImageTag:    imageTag,
